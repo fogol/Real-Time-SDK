956e2ee081a2a7566a5f94f4c42f94caeccb5a80
Mykhailishen <dmytro.mykhailishen@lseg.com>
2025-10-15 18:20:56 +0300

    RTSDK-10152: Fsanitize options to be added to CMake build as an option

--- CMake/rcdevCompilerOptions.cmake
+++ CMake/rcdevCompilerOptions.cmake
@@ -96,15 +96,19 @@ if( UNIX )
         set(_compilerBitFlags "-m32")
     endif()
 
+    if( DEFINED BUILD_SANITIZERS )
+        set ( CMAKE_SANITIZER_FLAGS "-fsanitize=address,undefined,leak -fno-sanitize=alignment" )
+    endif()
+
 	# flags for C
-    set( RCDEV_C_FLAGS_INIT "${_compilerBitFlags} -D_DEFAULT_SOURCE=1  -DLinux -DLINUX -Dx86_Linux_4X -Dx86_Linux_5X -Dx86_Linux_6X -DLinuxVersion=${RCDEV_HOST_SYSTEM_FLAVOR_REL} -pthread -D_iso_stdcpp_ -D_POSIX_SOURCE=1 -D_POSIX_C_SOURCE=199506L -D_XOPEN_SOURCE=500 -D_GNU_SOURCE" CACHE STRING "" FORCE )
+    set( RCDEV_C_FLAGS_INIT "${_compilerBitFlags} -D_DEFAULT_SOURCE=1  -DLinux -DLINUX -Dx86_Linux_4X -Dx86_Linux_5X -Dx86_Linux_6X -DLinuxVersion=${RCDEV_HOST_SYSTEM_FLAVOR_REL} -pthread -D_iso_stdcpp_ -D_POSIX_SOURCE=1 -D_POSIX_C_SOURCE=199506L -D_XOPEN_SOURCE=500 -D_GNU_SOURCE ${CMAKE_SANITIZER_FLAGS}" CACHE STRING "" FORCE )
 
     # Suppress Clang formatting warnings only
     if ( CMAKE_C_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
         set ( CMAKE_FLAGS_CLANG "-fno-omit-frame-pointer -Wno-format -Wno-format-security -Wno-parentheses -Wno-empty-body -Wno-unused-value -Wno-comment -Wno-logical-op-parentheses -Wno-dangling-else -Wno-constant-logical-operand -Wno-macro-redefined  -Wno-pointer-sign -Wno-constant-conversion -Wno-switch -Wno-enum-conversion -Wno-sizeof-pointer-memaccess -Wno-null-dereference -Wno-fortify-source -Wno-undefined-bool-conversion" CACHE STRING ""  FORCE )
     endif()
 
-    if(DEFINED BUILD_CODE_COVERAGE)
+    if( DEFINED BUILD_CODE_COVERAGE )
         set ( CMAKE_CODE_COVERAGE_FLAGS "-fprofile-arcs -ftest-coverage -O0 -g" )
     endif()
 
@@ -121,7 +125,7 @@ if( UNIX )
 	endif()
 
 	# flags for C++
-    set( RCDEV_CXX_FLAGS_INIT "${_compilerBitFlags} -DLinux -DLINUX -Dx86_Linux_4X -Dx86_Linux_5X -Dx86_Linux_6X -DLinuxVersion=${RCDEV_HOST_SYSTEM_FLAVOR_REL} -Wno-ctor-dtor-privacy -Wno-deprecated -std=c++11 -pthread  -D_iso_stdcpp_ -D_DEFAULT_SOURCE=1 -D_POSIX_SOURCE=1 -D_POSIX_C_SOURCE=199506L -D_XOPEN_SOURCE=500 -D_GNU_SOURCE"  CACHE STRING "" FORCE)
+    set( RCDEV_CXX_FLAGS_INIT "${_compilerBitFlags} -DLinux -DLINUX -Dx86_Linux_4X -Dx86_Linux_5X -Dx86_Linux_6X -DLinuxVersion=${RCDEV_HOST_SYSTEM_FLAVOR_REL} -Wno-ctor-dtor-privacy -Wno-deprecated -std=c++11 -pthread  -D_iso_stdcpp_ -D_DEFAULT_SOURCE=1 -D_POSIX_SOURCE=1 -D_POSIX_C_SOURCE=199506L -D_XOPEN_SOURCE=500 -D_GNU_SOURCE ${CMAKE_SANITIZER_FLAGS}"  CACHE STRING "" FORCE)
 	if ( ${CMAKE_BUILD_TYPE} STREQUAL "Optimized" )
 		set ( CMAKE_CXX_FLAGS "${RCDEV_CXX_FLAGS_INIT} -DNDEBUG -O3 -fbuiltin ${CMAKE_FLAGS_CLANG}" CACHE STRING ""  FORCE)
 	elseif ( ${CMAKE_BUILD_TYPE} STREQUAL "Debug" )
