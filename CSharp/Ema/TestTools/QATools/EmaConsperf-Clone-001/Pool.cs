e6fd1efb9c3a24e92a4e471fbe2e9d81fb9e56b7
Tymur Baksheiev <tymur.baksheiev@lseg.com>
2025-12-09 14:14:31 +0200

    RTSDK-10383. Updated copyright according to changes since last copyright update.

--- CSharp/Ema/TestTools/QATools/EmaConsperf-Clone-001/Pool.cs
+++ CSharp/Ema/TestTools/QATools/EmaConsperf-Clone-001/Pool.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections.Concurrent;
+/*|-----------------------------------------------------------------------------
+ *|            This source code is provided under the Apache 2.0 license
+ *|  and is provided AS IS with no warranty or guarantee of fit for purpose.
+ *|                See the project's LICENSE.md for details.
+ *|           Copyright (C) 2025 LSEG. All rights reserved.
+ *|-----------------------------------------------------------------------------
+ */
+
+using System.Collections.Concurrent;
 
 namespace LSEG.Ema.PerfTools.ConsPerf;
 
--- CSharp/Eta/Src/Tests/Xunit/CollectionAssertUtilTests.cs
+++ CSharp/Eta/Src/Tests/Xunit/CollectionAssertUtilTests.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*|-----------------------------------------------------------------------------
+ *|            This source code is provided under the Apache 2.0 license
+ *|  and is provided AS IS with no warranty or guarantee of fit for purpose.
+ *|                See the project's LICENSE.md for details.
+ *|           Copyright (C) 2025 LSEG. All rights reserved.
+ *|-----------------------------------------------------------------------------
+ */
+
+using System;
 using Xunit;
 using Xunit.Sdk;
 
